bc720b15465a5372b0bd621f3f281e2ad69d4cf8
DonWirson <twilliamson.valdes@gmail.com>
2025-01-03 19:13:21 -0300

    Initial Commit

---
+++ src/main/java/com/detocris/perdidog/PerdidogApplication.java
@@ -0,0 +1,13 @@
+package com.detocris.perdidog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerdidogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerdidogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/detocris/perdidog/PerdidogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.detocris.perdidog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerdidogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
